8e7b5015cfaf6b3d58461d47a16da018886ff29a
Douglas Oak <ramadawn@gmail.com>
2019-01-28 03:28:06 -0800

    Code and IMG : Question 3.18

---
+++ assignment1/_3_18.java
@@ -0,0 +1,39 @@
+/*
+Name: Douglas Oak
+
+Student# : Not Available due to post in public repository
+
+Algorithm Description:testing output
+
+
+No functions or external packages were used as part of this program
+
+Version: 0.1.0.0
+
+*/
+
+
+import java.util.*;
+
+public class _3_18 {
+	
+	public static void main(String[] args) {
+		
+		int x = 1;
+	
+		System.out.println("This program will calculated total money after term of investment");
+		System.out.println();
+		
+		System.out.println("(true) && (3 > 4) = " + ((true) && (3 > 4)));
+		System.out.println("!(x > 0) && (x > 0) = " + (!(x > 0) && (x > 0))); 
+		System.out.println("(x > 0) || (x < 0) = " + ((x > 0) || (x < 0)));
+		System.out.println("(x != 0) || (x == 0) = " + ((x != 0) || (x == 0))); 
+		System.out.println("(x >= 0) || (x < 0) = " + ((x >= 0) || (x < 0)));
+		System.out.println("(x != 1) == !(x == 1) = " + ((x != 1) == !(x == 1)));
+		
+		
+		
+	
+		
+	}
+}
