a9922487fc4716486815373aec94f0b9e6052c8b
Douglas Oak <ramadawn@gmail.com>
2019-01-28 03:38:27 -0800

    Code and IMG: Question 3.25

---
+++ assignment1/_3_25.java
@@ -0,0 +1,44 @@
+/*
+Name: Douglas Oak
+
+Student# : Not Available due to post in public repository
+
+Algorithm Description:testing output
+
+
+No functions or external packages were used as part of this program
+
+Version: 0.1.0.0
+
+*/
+
+
+import java.util.*;
+
+public class _3_25 {
+	
+	public static void main(String[] args) {
+		
+		int test = 14;
+		boolean result;
+		
+		if (test > 13 && test < 18){
+			
+			result = true;
+			
+		}
+		
+		else { result = false;}
+		
+		
+	
+		System.out.println("value = " + test + " result = " + result );
+		
+		
+		
+		
+		
+	
+		
+	}
+}
